fa9dfec217f20e08cb81ceaa1fb45d2b91e2062d
Djafta Muendane <djafta.n.m@gmail.com>
2023-08-10 13:08:05 +0200

    docs: add comments and improve reading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,23 @@
 "use client";
+import {ReactNode} from "react";
 
+// styles
 import './globals.css'
+
+// fonts
 import {Inter} from 'next/font/google'
-import {AuthProvider} from "@/context/authorization";
+
+// providers
 import {ApolloProvider} from "@apollo/client";
+import {AuthProvider} from "@/context/authorization";
 import {Providers} from "@/app/providers";
+
+// clients
 import {client} from "@/apollo/client";
 
 const inter = Inter({subsets: ['latin']})
 
-export default function RootLayout({children,}: { children: React.ReactNode }) {
+export default function RootLayout({children,}: { children: ReactNode }) {
 
     return (
         <html lang="en">
