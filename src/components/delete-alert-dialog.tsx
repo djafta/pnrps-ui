cec86b5569003249f50d7b6d33948b6bd650a472
Djafta Muendane <djafta.n.m@gmail.com>
2023-08-15 14:30:12 +0200

    upgrade: add dialog components for alert and deletion

---
+++ src/components/delete-alert-dialog.tsx
@@ -0,0 +1,60 @@
+import {useRef, useState} from "react";
+
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogCloseButton,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogOverlay
+} from "@chakra-ui/modal";
+
+import {Button, UseModalProps} from "@chakra-ui/react";
+
+
+export interface DeleteAlertDialogProps extends UseModalProps {
+    readonly title: string
+    readonly description: string
+    readonly onConfirm: () => void
+}
+
+export function DeleteAlertDialog({title, description, onConfirm, isOpen, onClose}: DeleteAlertDialogProps) {
+    const cancelRef = useRef<HTMLButtonElement | null>(null);
+    const [isLoading, setLoading] = useState(false);
+
+    async function handleConfirmClick() {
+        setLoading(true);
+        await onConfirm()
+        setLoading(false);
+    }
+
+    return (
+        <AlertDialog
+            motionPreset='slideInBottom'
+            onClose={onClose}
+            isOpen={isOpen}
+            isCentered
+            leastDestructiveRef={cancelRef}
+        >
+            <AlertDialogOverlay/>
+
+            <AlertDialogContent>
+                <AlertDialogHeader>{title}</AlertDialogHeader>
+                <AlertDialogCloseButton/>
+                <AlertDialogBody>
+                    {description}
+                </AlertDialogBody>
+                <AlertDialogFooter>
+                    <Button ref={cancelRef} onClick={onClose}>
+                        Não
+                    </Button>
+                    <Button isLoading={isLoading} onClick={handleConfirmClick} variant={"outline"} colorScheme='red'
+                            ml={3}>
+                        Sim
+                    </Button>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
