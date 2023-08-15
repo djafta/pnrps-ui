367ac8946e77690032842c24d23f20582f2e6e12
Djafta Muendane <djafta.n.m@gmail.com>
2023-08-15 14:37:28 +0200

    update: add skelton data to use in skelton components while fetching

---
+++ src/skelton-data.ts
@@ -0,0 +1,212 @@
+export const fakeFields = [
+    {
+        id: "1",
+        name: "",
+        code: ""
+    },
+    {
+        id: "2",
+        name: "",
+        code: ""
+    },
+    {
+        id: "3",
+        name: "",
+        code: ""
+    },
+    {
+        id: "4",
+        name: "",
+        code: ""
+    },
+    {
+        id: "5",
+        name: "",
+        code: ""
+    },
+    {
+        id: "6",
+        name: "",
+        code: ""
+    },
+    {
+        id: "7",
+        name: "",
+        code: ""
+    }
+]
+
+export const fakeClassifications = [
+    {
+        id: "1",
+        name: "",
+        code: ""
+    },
+    {
+        id: "2",
+        name: "",
+        code: ""
+    },
+    {
+        id: "3",
+        name: "",
+        code: ""
+    }
+]
+
+export const fakeSamples = [
+    {
+        id: "1",
+        name: "",
+        code: ""
+    },
+    {
+        id: "2",
+        name: "",
+        code: ""
+    },
+    {
+        id: "3",
+        name: "",
+        code: ""
+    }
+]
+
+export const fakeScopes = [
+    {
+        id: "1",
+        name: "",
+        code: "",
+        description: ""
+    },
+    {
+        id: "2",
+        name: "",
+        code: "",
+        description: ""
+    },
+    {
+        id: "3",
+        name: "",
+        code: "",
+        description: ""
+    }
+]
+
+export const fakeDocuments = [
+    {
+        id: "1",
+        name: "",
+        code: ""
+    },
+    {
+        id: "2",
+        name: "",
+        code: ""
+    },
+    {
+        id: "3",
+        name: "",
+        code: ""
+    }
+]
+
+export const fakeCountries = [
+    {
+        id: "1",
+        name: "",
+        code: "",
+        regions: []
+    },
+    {
+        id: "2",
+        name: "",
+        code: "",
+        regions: []
+    },
+    {
+        id: "3",
+        name: "",
+        code: "",
+        regions: []
+    }
+]
+
+export const fakeBodies = [
+    {
+        id: "1",
+        name: "",
+        code: "",
+        type: "",
+        description: ""
+    },
+    {
+        id: "2",
+        name: "",
+        code: "",
+        type: "",
+        description: ""
+    },
+    {
+        id: "3",
+        name: "",
+        code: "",
+        type: "",
+        description: ""
+    }
+]
+
+
+export const fakeFinanciers = [
+    {
+        id: "1",
+        name: "",
+        description: ""
+    },
+    {
+        id: "2",
+        name: "",
+        description: ""
+    },
+    {
+        id: "3",
+        name: "",
+        description: ""
+    }
+]
+
+export const fakeFinancingTypes = [
+    {
+        id: "1",
+        name: "",
+        description: ""
+    },
+    {
+        id: "2",
+        name: "",
+        description: ""
+    },
+    {
+        id: "3",
+        name: "",
+        description: ""
+    }
+]
+
+export const fakeResearcherRoles = [
+    {
+        id: "1",
+        name: "",
+        description: ""
+    },
+    {
+        id: "2",
+        name: "",
+        description: ""
+    },
+    {
+        id: "3",
+        name: "",
+        description: ""
+    }
+]
