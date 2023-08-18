0a0a4afe33aed2bb4133e4debb709d1e38da0f9a
Djafta Muendane <djafta.n.m@gmail.com>
2023-08-18 10:41:10 +0200

    update: fix error of too many rendering on generate mta

--- src/app/printing/[research]/mta/page.tsx
+++ src/app/printing/[research]/mta/page.tsx
@@ -1,96 +1,84 @@
 "use client"
 
-import {Document, Image, Page, PDFViewer, Text, View} from "@react-pdf/renderer"
+import {Document, Page, PDFViewer, Text, View} from "@react-pdf/renderer"
 import {Header} from "@/app/printing/[research]/header";
-import {useEffect, useState} from "react";
-import {Collaboration, Research} from "@/models";
+import {useMemo} from "react";
+import {Research} from "@/models";
 import {useQuery} from "@apollo/client";
 import {GET_RESEARCH_RC_QUERY} from "@/apollo";
 import {Spinner} from "@chakra-ui/react";
 import {styles} from "@/app/printing/[research]/styles";
-import Qrcode from "qrcode";
-import {useDefault} from "@/hooks/default";
 
 const SPACE = " "
-export default async function ResearchPrint({params}: { params: { research: string, doc: string } }) {
-    const {isDefault} = useDefault()
-    const [research, setResearch] = useState<Research | null>(null)
+export default function ResearchPrint({params}: { params: { research: string, doc: string } }) {
     const getResearchRCQuery = useQuery(GET_RESEARCH_RC_QUERY, {
         variables: {
             id: params.research
         }
     });
-
-    useEffect(() => {
-        if (getResearchRCQuery.data?.getResearchRC) {
-            setResearch(getResearchRCQuery.data.getResearchRC)
-        }
+    const research = useMemo(() => {
+        return getResearchRCQuery.data?.getResearchRC as Research
     }, [getResearchRCQuery])
 
-    if (global.window) {
-        if (global.window && research) {
-            const qrcode = await Qrcode.toDataURL(`${window.location.origin}/${research.id}`)
-            const {researcher} = research.collaborations.find(({role}) => isDefault(role)) as Collaboration
-
-            return (
-                <div className={"fixed w-full h-full flex"} suppressHydrationWarning>
-                    <PDFViewer width={"100%"} height={"100%"}>
-                        <Document>
-                            <Page
-                                size={"A4"}
-                                style={{
-                                    padding: "25mm 20mm",
-                                    position: "relative",
-                                }}
-                            >
-                                <Header/>
+    if (global.window && research) {
+        return (
+            <div className={"fixed w-full h-full flex"}>
+                <PDFViewer width={"100%"} height={"100%"}>
+                    <Document>
+                        <Page
+                            size={"A4"}
+                            style={{
+                                padding: "25mm 20mm",
+                                position: "relative",
+                            }}
+                        >
+                            <Header/>
+                            <View style={{
+                                ...styles.body,
+                                ...styles.mt50
+                            }}>
                                 <View style={{
-                                    ...styles.body,
-                                    ...styles.mt50
+                                    ...styles.header
                                 }}>
-                                    <View style={{
-                                        ...styles.header
-                                    }}>
-                                        <Text>
-                                            AUTORIZAÇÃO PARA TRANSFERÊNCIA DE AMOSTRAS BIOLÓGICAS
-                                        </Text>
-                                    </View>
+                                    <Text>
+                                        AUTORIZAÇÃO PARA TRANSFERÊNCIA DE AMOSTRAS BIOLÓGICAS
+                                    </Text>
+                                </View>
 
-                                    <View style={{
-                                        ...styles.mt10
+                                <View style={{
+                                    ...styles.mt10
+                                }}>
+                                    <Text>
+                                        É autorizada a transferência de amostras biológicas, referente ao
+                                        projecto/protocolo
+                                        intitulado `&quot;<Text>{research.title}</Text>`&quot; com
+                                        registro <Text>{research.approval.code} </Text>
+                                        nas quantidades abaixo descritas:
+                                    </Text>
+                                </View>
+                                <View>
+                                    <Text style={{
+                                        ...styles.textCenter,
+                                        ...styles.mt50
                                     }}>
-                                        <Text>
-                                            É autorizada a transferência de amostras biológicas, referente ao
-                                            projecto/protocolo
-                                            intitulado `&quot;<Text>{research.title}</Text>`&quot; com
-                                            registro <Text>{research.approval.code} </Text>
-                                            nas quantidades abaixo descritas:
-                                        </Text>
-                                    </View>
-                                    <View>
-                                        <Text style={{
-                                            ...styles.textCenter,
-                                            ...styles.mt50
-                                        }}>
-                                            A Direcção-Geral
-                                        </Text>
+                                        A Direcção-Geral
+                                    </Text>
 
-                                        <Text
+                                    <Text
                                         style={{
                                             ...styles.mt30,
                                             ...styles.textCenter
                                         }}
-                                        >
-                                            _________________________________
-                                        </Text>
-                                    </View>
+                                    >
+                                        _________________________________
+                                    </Text>
                                 </View>
-                            </Page>
-                        </Document>
-                    </PDFViewer>
-                </div>
-            )
-        }
+                            </View>
+                        </Page>
+                    </Document>
+                </PDFViewer>
+            </div>
+        )
     }
 
     return (
--- src/app/printing/[research]/report/page.tsx
+++ src/app/printing/[research]/report/page.tsx
@@ -17,78 +17,74 @@ export default function ResearchPrint({params}: { params: { research: string, do
         }
     });
 
-    console.log(params)
-
     useEffect(() => {
         if (getResearchReportQuery.data?.getResearchReport) {
             setResearch(getResearchReportQuery.data.getResearchReport)
         }
     }, [getResearchReportQuery])
 
-    if (global.window) {
-        if (global.window && research) {
-            return (
-                <div className={"fixed w-full h-full flex"} suppressHydrationWarning>
-                    <PDFViewer width={"100%"} height={"100%"}>
-                        <Document>
-                            <Page
-                                size={"A4"}
-                                style={{
-                                    padding: "25mm 20mm",
-                                    position: "relative",
-                                }}
-                            >
-                                <Header/>
+    if (global.window && research) {
+        return (
+            <div className={"fixed w-full h-full flex"} suppressHydrationWarning>
+                <PDFViewer width={"100%"} height={"100%"}>
+                    <Document>
+                        <Page
+                            size={"A4"}
+                            style={{
+                                padding: "25mm 20mm",
+                                position: "relative",
+                            }}
+                        >
+                            <Header/>
+                            <View style={{
+                                ...styles.body,
+                                ...styles.mt50
+                            }}>
+                                <View style={styles.fontBold}>
+                                    <Text>Exmo Sr(a)</Text>
+                                    <Text>Instituição</Text>
+                                </View>
+
                                 <View style={{
-                                    ...styles.body,
-                                    ...styles.mt50
+                                    ...styles.flex,
+                                    ...styles.flexRow,
+                                    ...styles.justifyBetween,
+                                    ...styles.mt10
                                 }}>
                                     <View style={styles.fontBold}>
-                                        <Text>Exmo Sr(a)</Text>
-                                        <Text>Instituição</Text>
+                                        <Text>Título da Pesquisa: <Text
+                                            style={styles.fontNormal}>{research.title}</Text></Text>
+                                        <Text>Código: <Text style={styles.fontNormal}>{research.code}</Text></Text>
                                     </View>
-
-                                    <View style={{
-                                        ...styles.flex,
-                                        ...styles.flexRow,
-                                        ...styles.justifyBetween,
-                                        ...styles.mt10
-                                    }}>
-                                        <View style={styles.fontBold}>
-                                            <Text>Título da Pesquisa: <Text
-                                                style={styles.fontNormal}>{research.title}</Text></Text>
-                                            <Text>Código: <Text style={styles.fontNormal}>{research.code}</Text></Text>
-                                        </View>
-                                        <View style={styles.fontBold}>
-                                            <Text>Data de Início: <Text
-                                                style={styles.fontNormal}>{research.startDate && new Date(research.startDate).toISOString().split("T")[0]}</Text></Text>
-                                            <Text>Data de Fim: <Text
-                                                style={styles.fontNormal}>{research.endDate && new Date(research.endDate).toISOString().split("T")[0]}</Text></Text>
-                                        </View>
-                                    </View>
-
-                                    <View style={{
-                                        ...styles.fontBold
-                                    }}>
-                                        <Text>Ponto de situação: <Text style={styles.fontNormal}>Informação
-                                            indisponível</Text></Text>
-                                        <Text>Faixa etária: <Text style={styles.fontNormal}>Informação
-                                            indisponível</Text></Text>
-                                        <Text>Área de abrangência: <Text style={styles.fontNormal}>{
-                                            research.province ? "Provincial" : research.region ? "Regional" : "Multicêntrica"
-                                        }</Text></Text>
-                                        <Text>Classificação do tipo de pesquisa: <Text
-                                            style={styles.fontNormal}>{research.classification?.name}</Text></Text>
-                                        <Text>Área da pesquisa: <Text
-                                            style={styles.fontNormal}>{research.field?.name}</Text></Text>
+                                    <View style={styles.fontBold}>
+                                        <Text>Data de Início: <Text
+                                            style={styles.fontNormal}>{research.startDate && new Date(research.startDate).toISOString().split("T")[0]}</Text></Text>
+                                        <Text>Data de Fim: <Text
+                                            style={styles.fontNormal}>{research.endDate && new Date(research.endDate).toISOString().split("T")[0]}</Text></Text>
                                     </View>
                                 </View>
-                            </Page>
-                        </Document>
-                    </PDFViewer>
-                </div>
-            )
-        }
+
+                                <View style={{
+                                    ...styles.fontBold
+                                }}>
+                                    <Text>Ponto de situação: <Text style={styles.fontNormal}>Informação
+                                        indisponível</Text></Text>
+                                    <Text>Faixa etária: <Text style={styles.fontNormal}>Informação
+                                        indisponível</Text></Text>
+                                    <Text>Área de abrangência: <Text style={styles.fontNormal}>{
+                                        research.province ? "Provincial" : research.region ? "Regional" : "Multicêntrica"
+                                    }</Text></Text>
+                                    <Text>Classificação do tipo de pesquisa: <Text
+                                        style={styles.fontNormal}>{research.classification?.name}</Text></Text>
+                                    <Text>Área da pesquisa: <Text
+                                        style={styles.fontNormal}>{research.field?.name}</Text></Text>
+                                </View>
+                            </View>
+                        </Page>
+                    </Document>
+                </PDFViewer>
+            </div>
+        )
     }
 
     return (
