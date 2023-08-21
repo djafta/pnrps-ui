024e7d1e5b836f51f593aaf79d8057557c9ae00c
Djafta Muendane <djafta.n.m@gmail.com>
2023-08-21 13:12:02 +0200

    update: add types for birthDate and sex on user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,5 +3,7 @@ export interface User {
     readonly email: string
     readonly lastName: string
     readonly firstName: string
+    readonly birthDate: string
+    readonly sex: string
     readonly features: string[]
 }
