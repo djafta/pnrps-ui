f59dced2dc5e0153fef8572e4bcf4da2961d3d17
Djafta Muendane <djafta.n.m@gmail.com>
2023-08-22 16:37:48 +0200

    upgrade: make research modal read only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import React, {useEffect, useMemo} from "react";
-import {BiDownload} from "react-icons/bi";
+import React, {useEffect, useMemo, useState} from "react";
 import {PublicHeader} from "@/components/header/public";
 
 import {
@@ -31,33 +30,56 @@ import {
     useDisclosure
 } from "@chakra-ui/react";
 import {Research} from "@/models";
-import {useAuth} from "@/hooks/auth";
-import {useRouter} from "next/navigation";
-import {useQuery} from "@apollo/client";
-import {LIST_RESEARCHES_QUERY} from "@/apollo";
+import {useLazyQuery, useQuery} from "@apollo/client";
+import {HAS_APPROVAL_QUERY, LIST_RESEARCHES_QUERY} from "@/apollo";
+import {ViewResearchDataCard} from "@/app/researches/cards/view-research-data-card";
+import {ViewResearchGeographicDataCard} from "@/app/researches/cards/view-research-geographic-data-card";
+import {ViewResearchersCard} from "@/app/researches/cards/view-researchers-card";
+import {ViewFinancingCard} from "@/app/researches/cards/view-financing-card";
+import {ViewResearchApprovalCard} from "@/app/researches/cards/view-research-approval-card";
+import {ViewResearchFilesCard} from "@/app/researches/cards/view-research-files-card";
+import {EditResearchAgreementsCard} from "@/app/dashboard/management/researches/cards/edit-research-agreements-card";
+import Link from "next/link";
+import {AiOutlineFilePdf} from "react-icons/ai";
 
 export default function Home() {
-    const {isOpen, onOpen, onClose} = useDisclosure();
-    const {user} = useAuth();
-    const router = useRouter();
-
     const listResearchesQuery = useQuery(LIST_RESEARCHES_QUERY, {
         pollInterval: 1000 * 30 // 30 seconds
     })
+
+    const [getResearchApproval] = useLazyQuery(HAS_APPROVAL_QUERY)
+    const [hasApproval, setHasApproval] = useState(false);
+
+    const [research, setResearch] = useState<Research>({} as Research);
 
     const researches = useMemo(() => {
         return (listResearchesQuery.data?.listResearches || []) as Research[]
     }, [listResearchesQuery])
 
+    const disclosure = useDisclosure();
+
     useEffect(() => {
-        if (user) {
-            router.push("/dashboard")
+        if (disclosure.isOpen && research) {
+            getResearchApproval({
+                variables: {
+                    id: research?.id
+                }
+            }).then(({data}) => {
+                setHasApproval(!!data?.getResearchApproval)
+            })
         }
-    }, [user, router])
+    }, [research, disclosure.isOpen, getResearchApproval])
 
     function handleResearchRowClick(research: Research | undefined) {
-        onOpen()
+        if (research) {
+            setResearch(research)
+            disclosure.onOpen()
+        }
     }
+
+    useEffect(() => {
+        setResearch(research => researches.find((r) => research?.id === r.id) as Research)
+    }, [researches])
 
     return (
         <>
@@ -92,35 +114,52 @@ export default function Home() {
                     </Card>
                 </div>
                 <div className={"p-2"}>
-                    <Modal isOpen={isOpen} onClose={onClose} size={"4xl"} isCentered={true}>
+                    <Modal isOpen={disclosure.isOpen} onClose={disclosure.onClose}
+                           size={"6xl"} scrollBehavior={"inside"}>
                         <ModalOverlay/>
                         <ModalContent>
-                            <ModalHeader>Detalhes da pesquisa</ModalHeader>
-                            <ModalCloseButton/>
+                            <ModalHeader>
+                                <ModalCloseButton className={"z-40"}/>
+                            </ModalHeader>
                             <ModalBody>
-                                <div className={"grid grid-cols-2 gap-6"}>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
-                                    <SkeletonText noOfLines={1} skeletonHeight={5}></SkeletonText>
+                                <div className={"flex flex-col gap-6"}>
+                                    <ViewResearchDataCard research={research} setResearch={setResearch}/>
+                                    <ViewResearchGeographicDataCard research={research} setResearch={setResearch}/>
+                                    <ViewResearchersCard research={research} setResearch={setResearch}/>
+                                    <ViewFinancingCard research={research} setResearch={setResearch}/>
+                                    <ViewResearchApprovalCard research={research} setResearch={setResearch}/>
+                                    <ViewResearchFilesCard research={research} setResearch={setResearch}/>
+                                    <EditResearchAgreementsCard research={research}/>
                                 </div>
                             </ModalBody>
                             <ModalFooter>
-                                <Skeleton>
-                                    <Button colorScheme={"teal"} variant={"outline"} mr={3} rightIcon={<BiDownload/>}>
-                                        Baixar
+                                <div className={"w-full flex gap-2 md:flex-row md:justify-end"}>
+                                    <Link href={`/printing/${research?.id}/report`} target={"_blank"}>
+                                        <Button colorScheme={"teal"} variant={"outline"}
+                                                rightIcon={<AiOutlineFilePdf/>}>
+                                            Imprimir Relatório
+                                        </Button>
+                                    </Link>
+                                    <Button
+                                        isDisabled={!hasApproval}
+                                        title={!hasApproval ? "Esta pesquisa ainda não foi aprovada" : "Imprimir Acordo de Transferência de Material"}
+                                        colorScheme={"teal"} variant={"outline"} rightIcon={<AiOutlineFilePdf/>}>
+                                        <Link href={hasApproval ? `/printing/${research?.id}/mta` : "#"}
+                                              target={hasApproval ? "_blank" : undefined}>
+                                            Imprimir MTA
+                                        </Link>
                                     </Button>
-                                </Skeleton>
+                                    <Button
+                                        isDisabled={!hasApproval}
+                                        title={!hasApproval ? "Esta pesquisa ainda não foi aprovada" : "Imprimir Confirmação de registro"}
+                                        colorScheme={"teal"} variant={"outline"} rightIcon={<AiOutlineFilePdf/>}>
+                                        <Link
+                                            href={hasApproval ? `/printing/${research?.id}/cr` : "#"}
+                                            target={hasApproval ? "_blank" : undefined}>
+                                            Imprimir CR
+                                        </Link>
+                                    </Button>
+                                </div>
                             </ModalFooter>
                         </ModalContent>
                     </Modal>
