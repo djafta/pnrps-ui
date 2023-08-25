d99c9dd403cc9e0df0cb15486727349d391e9a28
Djafta Muendane <djafta.n.m@gmail.com>
2023-08-25 08:24:08 +0200

    upgrade: add statistic to public portal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React, {useEffect, useMemo, useState} from "react";
 import {PublicHeader} from "@/components/header/public";
 
 import {
+    Box,
     Button,
     Card,
     FormControl,
@@ -15,14 +16,14 @@ import {
     ModalFooter,
     ModalHeader,
     ModalOverlay,
-    Skeleton,
+    Skeleton, SkeletonCircle,
     SkeletonText,
     Switch,
     Table,
     TableCaption,
     TableContainer,
     Tbody,
-    Td,
+    Td, Text,
     Th,
     Thead,
     Tooltip,
@@ -31,7 +32,7 @@ import {
 } from "@chakra-ui/react";
 import {Research} from "@/models";
 import {useLazyQuery, useQuery} from "@apollo/client";
-import {HAS_APPROVAL_QUERY, LIST_RESEARCHES_QUERY} from "@/apollo";
+import {GET_RESEARCH_STATISTIC, GET_USER_STATISTIC, HAS_APPROVAL_QUERY, LIST_RESEARCHES_QUERY} from "@/apollo";
 import {ViewResearchDataCard} from "@/app/researches/cards/view-research-data-card";
 import {ViewResearchGeographicDataCard} from "@/app/researches/cards/view-research-geographic-data-card";
 import {ViewResearchersCard} from "@/app/researches/cards/view-researchers-card";
@@ -43,6 +44,7 @@ import Link from "next/link";
 import {AiOutlineFilePdf} from "react-icons/ai";
 import {useRouter} from "next/navigation";
 import {useAuth} from "@/hooks/auth";
+import Highcharts from "highcharts";
 
 export default function Home() {
     const router = useRouter()
@@ -61,6 +63,14 @@ export default function Home() {
     }, [listResearchesQuery])
 
     const disclosure = useDisclosure();
+
+    const getResearchStatisticQuery = useQuery(GET_RESEARCH_STATISTIC, {
+        pollInterval: 1000 * 10
+    })
+    const researchStatistic = useMemo(() => {
+        return getResearchStatisticQuery.data?.getResearchStatistics
+    }, [getResearchStatisticQuery])
+
 
     useEffect(() => {
         if (disclosure.isOpen && research) {
@@ -85,7 +95,134 @@ export default function Home() {
         if (user) {
             router.push("/dashboard")
         }
-    }, [router, user])
+
+        try {
+            (async () => {
+                if (window.document.getElementById("monthly")) {
+                    // @ts-ignore
+                    Highcharts.chart('monthly', {
+                        chart: {
+                            zoomType: 'x'
+                        },
+                        title: {
+                            text: 'Pesquisas mensais',
+                            align: 'left'
+                        },
+                        subtitle: {
+                            text: document.ontouchstart === undefined ?
+                                'Click e araste na area do gráfico para zoom' : 'Pinch the chart to zoom in',
+                            align: 'left'
+                        },
+                        xAxis: {
+                            type: 'datetime'
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'Pesquisas'
+                            }
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        plotOptions: {
+                            area: {
+                                fillColor: {
+                                    linearGradient: {
+                                        x1: 0,
+                                        y1: 0,
+                                        x2: 0,
+                                        y2: 1
+                                    },
+                                    stops: [
+                                        //@ts-ignore
+                                        [0, Highcharts.getOptions().colors[0]],
+                                        //@ts-ignore
+                                        [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                                    ]
+                                },
+                                marker: {
+                                    radius: 2
+                                },
+                                lineWidth: 1,
+                                states: {
+                                    hover: {
+                                        lineWidth: 1
+                                    }
+                                },
+                                threshold: null
+                            }
+                        },
+
+                        series: [{
+                            type: 'area',
+                            name: 'Pesquisas',
+                            data: researchStatistic?.monthly
+                        }]
+                    });
+                }
+
+                if (window.document.getElementById("statuses")) {
+                    // @ts-ignore
+                    Highcharts.chart('statuses', {
+                        chart: {
+                            plotBackgroundColor: null,
+                            plotBorderWidth: null,
+                            plotShadow: false,
+                            type: 'pie'
+                        },
+                        title: {
+                            text: '',
+                            align: 'center'
+                        },
+                        tooltip: {
+                            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                        },
+                        accessibility: {
+                            point: {
+                                valueSuffix: '%'
+                            }
+                        },
+                        plotOptions: {
+                            pie: {
+                                allowPointSelect: true,
+                                cursor: 'pointer',
+                                borderRadius: 5,
+                                dataLabels: {
+                                    enabled: true,
+                                    format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                                    distance: -50,
+                                    filter: {
+                                        property: 'percentage',
+                                        operator: '>',
+                                        value: 4
+                                    }
+                                }
+                            }
+                        },
+                        series: [{
+                            name: 'Cerca de',
+                            data: [
+                                {
+                                    name: "Aprovadas",
+                                    y: researchStatistic?.approved,
+                                    // @ts-ignore
+                                    color: Highcharts.getOptions().colors[0]
+                                },
+                                {
+                                    name: "Não Aprovadas",
+                                    y: researchStatistic?.nonApproved,
+                                    // @ts-ignore
+                                    color: Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0.5).get('rgba').toString()
+                                }
+                            ]
+                        }]
+                    });
+                }
+
+            })();
+        } catch (e) {
+        }
+    }, [router, user, researchStatistic])
 
     useEffect(() => {
         setResearch(research => researches.find((r) => research?.id === r.id) as Research)
@@ -96,29 +233,85 @@ export default function Home() {
             <PublicHeader/>
             <main className={"pt-24 flex flex-col gap-10"}>
                 <div className={"p-2 grid grid-rows-3 gap-6 lg:grid-cols-3 lg:grid-rows-none"}>
-                    <Card className={"p-4"}>
-                        <SkeletonText height={"100px"} spacing={4} skeletonHeight={4} width={"200px"} noOfLines={2}>
+                    <Card className={"p-4 flex gap-4"}>
+                        <div className={"flex gap-2 items-center"}>
+                            <SkeletonCircle isLoaded={!!researchStatistic} width={"100px"} height={"100px"}>
+                                <Box className={"bg-blue-400 flex items-center justify-center"} rounded={"full"}
+                                     width={"100px"}
+                                     height={"100px"}>
+                                    <Text className={"text-white text-xl"}>
+                                        {researchStatistic?.total}
+                                    </Text>
+                                </Box>
+                            </SkeletonCircle>
+                            <Text className={"text-gray-700 font-bold text-2xl"}>
+                                Pesquisas
+                            </Text>
+                        </div>
+                        <SkeletonText isLoaded={!!researchStatistic} noOfLines={3} skeletonHeight={4}>
+                            <Box className={"flex items-center"}>
+                                <Text className={"text-gray-500 text-md"}>Total de pesquisas existentes</Text>
+                            </Box>
                         </SkeletonText>
                     </Card>
-                    <Card className={"p-4"}>
-                        <SkeletonText height={"100px"} spacing={4} skeletonHeight={4} width={"200px"} noOfLines={2}>
-
+                    <Card className={"p-4 flex gap-4"}>
+                        <div className={"flex gap-2 items-center"}>
+                            <SkeletonCircle isLoaded={!!researchStatistic} width={"100px"} height={"100px"}>
+                                <Box className={"bg-blue-400 flex items-center justify-center"} rounded={"full"}
+                                     width={"100px"}
+                                     height={"100px"}>
+                                    <Text className={"text-white text-xl"}>
+                                        {researchStatistic?.approved}
+                                    </Text>
+                                </Box>
+                            </SkeletonCircle>
+                            <Text className={"text-gray-700 font-bold text-2xl"}>
+                                Pesquisas Aprovadas
+                            </Text>
+                        </div>
+                        <SkeletonText isLoaded={!!researchStatistic} noOfLines={3} skeletonHeight={4}>
+                            <Box className={"flex items-center"}>
+                                <Text className={"text-gray-500 text-md"}>Total de pesquisas aprovadas</Text>
+                            </Box>
                         </SkeletonText>
                     </Card>
-                    <Card className={"p-4"}>
-                        <SkeletonText height={"100px"} spacing={4} skeletonHeight={4} width={"200px"} noOfLines={2}>
-
+                    <Card className={"p-4 flex gap-4"}>
+                        <div className={"flex gap-2 items-center"}>
+                            <SkeletonCircle isLoaded={!!researchStatistic} width={"100px"} height={"100px"}>
+                                <Box className={"bg-blue-400 flex items-center justify-center"} rounded={"full"}
+                                     width={"100px"}
+                                     height={"100px"}>
+                                    <Text className={"text-white text-xl"}>
+                                        {researchStatistic?.nonApproved}
+                                    </Text>
+                                </Box>
+                            </SkeletonCircle>
+                            <Text className={"text-gray-700 font-bold text-2xl"}>
+                                Pesquisas por Aprovar
+                            </Text>
+                        </div>
+                        <SkeletonText isLoaded={!!researchStatistic} noOfLines={3} skeletonHeight={4}>
+                            <Box className={"flex items-center"}>
+                                <Text className={"text-gray-500 text-md"}>Total de pesquisas em processo de
+                                    aprovação</Text>
+                            </Box>
                         </SkeletonText>
                     </Card>
                 </div>
                 <div className={"p-2"}>
-                    <Card className={"p-2"}>
-                        <div className={"flex flex-col gap-6 lg:flex-row"}>
-                            <Skeleton className={"w-full lg:h-[400px]"} width={"100%"} height={"300px"}>
+                    <Card className={"p-2 overflow-hidden"}>
+                        <div className={"flex flex-col gap-56 lg:flex-row lg:gap-6"}>
+                            <Skeleton isLoaded={!!researchStatistic} className={"w-full lg:h-[400px]"} width={"100%"}
+                                      height={"300px"}>
+                                <div id={"monthly"}>
 
+                                </div>
                             </Skeleton>
-                            <Skeleton className={"w-full lg:w-[48.5%] lg:h-[400px]"} height={"300px"}>
+                            <Skeleton isLoaded={!!researchStatistic} className={"w-full lg:w-[48.5%] lg:h-[400px]"}
+                                      height={`${!researchStatistic ? "300px" : "auto"}`}>
+                                <div id={"statuses"}>
 
+                                </div>
                             </Skeleton>
                         </div>
                     </Card>
